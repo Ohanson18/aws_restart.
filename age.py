7e6f7273e866517a20c09e90503a484b09f61e80
Ohanson18 <68284689+Ohanson18@users.noreply.github.com>
2022-06-10 10:21:21 +0100

    Update age.py

--- age.py
+++ age.py
@@ -1,6 +1,6 @@
-age =21
-if age >18
-   pritn("adult")
-   else;
-   print ("child")
-
+print("Hello how old are you ? ")
+age=int(input(" "))
+if age >= 18:
+   print("Your age shows you are adult")
+else:
+   print ("Sorry your age shows you are a child")
